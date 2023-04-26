eb1fb98043a19d5983d2c6c1b58bb895b041c937
Yugi Nova <yuginovaniac@gmail.com>
2023-04-26 17:18:14 +0700

    config routes for first deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import Welcome from './components/ChatRoom/Welcome';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/ChapApp",
     element: <PublicRoute component={<SignInPage/>}/>  
   },
   {
